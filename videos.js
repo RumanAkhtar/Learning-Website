e2b7ec1db09bdd4f91ff04466b38add4dc8240fd
RumanAkhtar <cseruman42@gmail.com>
2024-02-12 21:33:18 +0530

    first commit

---
+++ videos.js
@@ -0,0 +1,36 @@
+export default [
+    {
+      
+      src: "./image/v1.mp4",
+      title: "Learn React | What is React? | React js full introduction",
+    },
+    {
+      src: "./image/v2.mp4",
+      title: "C programming language Introduction",
+    },
+    {
+      src: "./image/v3.mp4",
+      title: "Next js full detail video",
+    },
+    {
+      src: "./image/v4.mp4",
+      title: "JavaScript for Beginner",
+    },
+    {
+      src: "./image/v2.mp4",
+      title: "Next js full detail video",
+    },
+    {
+      src: "./image/v1.mp4",
+      title: "JavaScript for Beginner",
+    },
+    {
+      src: "./image/v3.mp4",
+      title: "JavaScript for Beginner",
+    },
+    {
+      src: "./image/v2.mp4",
+      title: "JavaScript for Beginner",
+    },
+  ];
+  
